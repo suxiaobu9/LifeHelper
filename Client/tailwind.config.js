01757e4bdd95686c093754c4d177d9a6ed51a2ad
Ari <ari@miniasp.com>
2022-07-10 20:34:43 +0800

    upload menu

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    content: ["**/*.razor", "**/*.cshtml", "**/*.html", "**/*.cshtml"],
-    theme: {
-        extend: {
-            translate: {
-                '-50%': '-50%',
-            },
-            borderRadius: {
-                '50%': '50%'
-            }
-        },
+  content: ["**/*.razor", "**/*.cshtml", "**/*.html", "**/*.cshtml"],
+  theme: {
+    extend: {
+      translate: {
+        "-50%": "-50%",
+      },
+      borderRadius: {
+        "50%": "50%",
+      },
     },
-    plugins: [],
-}
+  },
+  plugins: [],
+};
